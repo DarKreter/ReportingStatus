33c1ef49d85295aa2471341eb9fd74d8bea8f256
DarKreter <darkreter@gmail.com>
2022-10-24 14:49:18 +0200

    discord writing on proper channel

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 import discord
 from unidecode import unidecode
 from client import client
+from globalVar import *
 
 
 # Called when bot is ready 
@@ -10,14 +11,22 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client))
     
     game = discord.Game("with your mom")
-    await client.change_presence(status=discord.Status.invisible, activity=game)
-
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+    await client.change_presence(status=discord.Status.online, activity=game)
     
-    await message.channel.send(message.content)
+    
+    for ch in client.get_guild(GuildID).channels:
+        if ch.id == ChannelID:
+            channel = ch
+
+    await channel.send("test")
+    
+    # embedVar = discord.Embed(title="tytul", description="123", color=0xe0a2e6)
+# if source != "":
+#     embedVar.add_field(name="Sosik:", value="source", inline=False)
+# embedVar.set_image(url = "https://www.cukierniasamanta.pl/sklep/wp-content/uploads/2020/03/paczek.jpg")
+    
+# await message.channel.send(embed=embedVar)
+
+# await message.channel.send(embed=embedVar)
 
     
--- report.py
+++ report.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
 from playlist_checker import check_playlists
+from client import client
+from TOKEN import TOKEN
 
-missing_files = check_playlists()
+
+# missing_files = check_playlists()
+
+missing_files = [('playlista1', 'video1'), ('playlista2', 'video2')]
 
 for file in missing_files:
     print(file)
+
+
+client.run(TOKEN)
+
 
 # List all files in folder
 # List all files in yt playlist
